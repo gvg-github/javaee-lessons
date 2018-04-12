4758c393ff64a5cd52a141dec49796f0bba4c32a
gvg2000 <gvg2000@yandex.ru>
2018-04-12 23:41:51 +0300

    Lesson 2

---
+++ src/main/java/ru/lesson2/servlet/CatalogServlet.java
@@ -0,0 +1,18 @@
+package ru.lesson2.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/catalog")
+public class CatalogServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        resp.getWriter().println("Hello");
+        req.setAttribute("namePage", "Catalog page");
+        req.getRequestDispatcher("main.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/java/ru/lesson2/servlet/MainServlet.java
@@ -0,0 +1,15 @@
+package ru.lesson2.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("namePage", "Main page");
+        req.getRequestDispatcher("main.jsp").forward(req, resp);
+    }
+}
